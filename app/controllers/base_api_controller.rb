147ea570a27599b5b82dce476a07141a44c51f9d
Rodrigo Merino <ebedmail@gmail.com>
2022-03-16 11:02:09 -0300

    feat(controller): adding new controller for apis with empty response

---
+++ app/controllers/base_api_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Base api to handle the common behaviour
+class BaseApiController < ApplicationController
+
+end
---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,15 @@
+class WeatherController < BaseApiController
+
+  def index
+    render json: 'ok'
+  end
+
+  def show
+    render json: 'ok'
+  end
+
+  def create
+    render json: 'ok'
+  end
+
+end
